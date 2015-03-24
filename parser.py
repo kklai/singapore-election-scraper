f0212c8fe35eb1d1ed4d16e4bc884e4ab27c522c
kklai <kkrebeccalai@gmail.com>
2015-03-24 02:08:42 -0400

    so close to getting this script to workgit status

--- parser.py
+++ parser.py
@@ -1,25 +1,76 @@
 import csv
 
-years = ["1955", "1959", "1963", "1968", "1972", "1980", "1984", "1988", "1991", "1997", "2001", "2006", "2011"]
+years = ["1959", "1963", "1968", "1972", "1980", "1984", "1988", "1991", "1997", "2001", "2006", "2011"]
 
-with open("results_by_party.csv", "w") as csvfile:
-  fieldNames = ["year", "AI", "BS", "CP", "DP", "DPP", "INDP", "JPS", "KURA", "LP", "LSP", "NSP", "PAP", "PF", "PKMS", "PMIP", "PP", "PRSSD", "RP", "SA", "SC", "SDA", "SDP", "SJP", "SLF", "SPA", "SPP", "SUF", "UF", "UNF", "UDP", "UPF", "UPP", "WP"]
+with open("results_by_seats.csv", "w") as csvfile:
+  fieldNames = ["year", "PAP", "SPP", "WP", "Others"]
   w = csv.DictWriter(csvfile, fieldNames)
   w.writeheader()
-
-  def writerows(year):
-    w.writerow({'year': year})
 
   def countVotes(year):
     with open("result_" + year + ".csv", "rb") as f:
       result = csv.reader(f, delimiter=',', quotechar='|')
-      for row in result:
-        party = row[1]
-        vote = row[3]
 
-        if party != "party":
-          # w.writerow({'year': year, party: sum()})
+      pap_vote = 0
+      spp_vote = 0
+      wp_vote = 0
+      others = 0
+
+      findWinner = []
+
+
+      for idx, row in enumerate(result):
+        if idx == 1:
+          currentDistrict = row[0].split("(")[0]
+
+        if row[1] != "party":
+          district = row[0].split("(")[0]
+          if currentDistrict == district:
+            findWinner.append([district, row[1], row[3]])
+          else:
+            if len(findWinner) == 1:
+              winner = row[1]
+            else:
+              votes = []
+              for x in findWinner:
+                votes.append(int(float(x[2])))
+              winner = findWinner[votes.index(max(votes))][1]
+
+            if winner == "PAP":
+              pap_vote += 1
+            elif winner == "SPP":
+              spp_vote += 1
+            elif winner == "WP":
+              wp_vote += 1
+            else:
+              others += 1
+
+            findWinner = []
+            currentDistrict = district
+            findWinner.append([district, row[1], row[3]])
+
+      w.writerow({'year': year, "PAP": pap_vote, "SPP": spp_vote, "WP": wp_vote, "Others": others})
           
 
+          
+
+
+      #   party = row[1]
+      #   vote = row[3]
+
+
+      #   if vote != "Uncontested" and vote != "votes":
+      #     if party == "PAP":
+      #       pap_vote += int(float(vote))
+      #     elif party == "SPP":
+      #       spp_vote += int(float(vote))
+      #     elif party == "WP":
+      #       wp_vote += int(float(vote))
+      #     else:
+      #       others += int(float(vote))
+
+      # w.writerow({'year': year, "PAP": pap_vote, "SPP": spp_vote, "WP": wp_vote, "Others": others})
+            
+
   for x in years:
-    writerows(x)
+    countVotes(x)
